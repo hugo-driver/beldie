18919874686a59489b19c1c7d7c05e926c8f7a23
hugo-driver <hubgit@fastmail.de>
2025-09-02 11:30:44 +0300

    Update meta.js

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -89,9 +89,4 @@ export const greenweb = {
   ],
   services: [{domain: 'netlify.com', serviceType: 'cdn'}]
 };
-export const viewRepo = {
-  // this is for the view/edit on github link. The value in the package.json will be pulled in.
-  allow: true,
-  infoText: 'View this page on GitHub'
-};
 export const easteregg = true;
